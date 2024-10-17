69d822cdbafa0da381790f64481c72a1c58d35d2
Robles27rowan <160294655+Robles27rowan@users.noreply.github.com>
2024-10-17 13:22:50 -0400

    Update main.py

--- DGR/main.py
+++ DGR/main.py
@@ -40,58 +40,122 @@ def handle_inputs():
     check_for_errors(args, **kwargs)                 # -check whether incompatible options are selected
     return args
 
-def set_saliency(self, mode):
-        self.saliency = mode
+def get_indices(dataset,class_name):
+    indices =  []
+    for i in range(len(dataset.targets)):
+      for j in class_name:
+        if dataset.targets[i] == j:
+            indices.append(i)
+    return indices
+    
+    
+def load_saliency_data(desired_classes, imgs_per_class):
+    transform = transforms.Compose(
+    [transforms.ToTensor(),
+    #transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
+    ])
 
-def create_saliency_map(model, saliency_loader, pred, ses):
-    ##### Create Saliency Maps
-    data_iter = iter(saliency_loader)
-    sal_imgs, sal_labels = next(data_iter)
+    if not os.path.isdir("SaliencyMaps/" + args.dataset):
+        mkdir_p("SaliencyMaps/" + args.dataset)
+    
+    saliencySet = torch.utils.data.Dataset()
+    if args.dataset == "mnist":       
+        saliencySet = datasets.MNIST(root='SaliencyMaps/Datasets/mnist/', train=False,
+                  download=True, transform=transform)
+
+    idx = get_indices(saliencySet,desired_classes)
+
+    subset = Subset(saliencySet, idx)
+
+    # Create a DataLoader for the subset
+    saliencyLoader = DataLoader(subset, batch_size=args.test_batch)
+
+    dataiter = iter(saliencyLoader)
+    images, labels = next(dataiter)
+
+    salIdx = []
+    salLabels = []
+    for i in range(len(desired_classes)):
+      num=0
+      while len(salIdx) < imgs_per_class*(i+1):
+        if labels[num]==desired_classes[i]:
+          salIdx.append(num)
+          salLabels.append(desired_classes[i])
+        num += 1
+    salImgs = images[salIdx]
+    
+    return salImgs, torch.tensor(salLabels), saliencySet.classes
+    
+    
+    
+def create_saliency_map(model, ses, desired_classes, imgs_per_class):
+    
+    sal_imgs, sal_labels, classes = load_saliency_data(desired_classes, imgs_per_class)
     sal_imgs, sal_labels = sal_imgs.cuda(), sal_labels.cuda()
-    predicted = pred.squeeze()
+    
+    outputs2, outputs = model(sal_imgs)
+    pred = torch.argmax(outputs2[:,0:args.class_per_task*(1+args.sess)], 1, keepdim=False)
+    predicted = pred.squeeze()        
     
     
     model.set_saliency(True)
     saliency = Saliency(model)
-    #print(sal_labels)
     
-    fig, ax = plt.subplots(1,4,figsize=(10,4))
-    for ind in range(0,2):
-        input = sal_imgs[ind+1].unsqueeze(0)
+    fig, ax = plt.subplots(2,2*imgs_per_class,figsize=(15,5))
+    for ind in range(2*imgs_per_class):
+        input = sal_imgs[ind].unsqueeze(0)
         input.requires_grad = True
 
-        grads = saliency.attribute(input, target=sal_labels[ind+1].item(), abs=False)
+        grads = saliency.attribute(input, target=sal_labels[ind].item(), abs=False)
         squeeze_grads = grads.squeeze().cpu().detach()
         squeeze_grads = torch.unsqueeze(squeeze_grads,0).numpy()
         grads = np.transpose(squeeze_grads, (1, 2, 0))
-
-        print('Truth:', classes[sal_labels[ind+1]])
-        print('Predicted:', classes[predicted[ind+1]])
         
-        original_image = np.transpose((sal_imgs[ind+1].cpu().detach().numpy() / 2) + 0.5, (1, 2, 0))       
+        print('Truth:', classes[sal_labels[ind]])
+        print('Predicted:', classes[predicted[ind]])
 
+
+        #original_image = np.transpose((sal_imgs[ind].cpu().detach().numpy() / 2) + 0.5, (1, 2, 0))
+        original_image = np.transpose((sal_imgs[ind].cpu().detach().numpy()), (1, 2, 0))
+        
+        
         methods=["original_image","blended_heat_map"]
         signs=["all","absolute_value"]
         titles=["Original Image","Saliency Map"]
         colorbars=[False,True]
-        for i in range(0,2):
-            plt_fig_axis = (fig,ax[2*ind+i])
+
+        # Check if image was misclassified
+        if predicted[ind] != sal_labels[ind]: cmap = "Reds" 
+        else: cmap = "Blues"
+
+
+        if ind > imgs_per_class-1:
+            row = 1
+            ind = ind - imgs_per_class
+        else: row = 0
+
+        for i in range(2):
+            plt_fig_axis = (fig,ax[row][2*ind+i])
             if i==1:
                 _ = viz.visualize_image_attr(grads, original_image,
                                             method=methods[i],
                                             sign=signs[i],
+                                            fig_size=(4,4),
                                             plt_fig_axis=plt_fig_axis,
+                                            cmap=cmap,
                                             show_colorbar=colorbars[i],
                                             title=titles[i])
             else:
-                ax[2*ind+i].imshow(original_image, cmap='gray')
-                ax[2*ind+i].set_title('Original Image')
-                ax[2*ind+i].tick_params(left = False, right = False , labelleft = False , 
+                ax[row][2*ind+i].imshow(original_image, cmap='gray')
+                ax[row][2*ind+i].set_title('Original Image')
+                ax[row][2*ind+i].tick_params(left = False, right = False , labelleft = False , 
                                 labelbottom = False, bottom = False) 
-            
-    fig.savefig(f"SaliencyMaps/MNIST/Sess{ses}SalMap.png")
-    fig.show()  
+    
+    fig.tight_layout()
+    fig.savefig(f"SaliencyMaps/{args.dataset}/Sess{ses}SalMap.png")
+    fig.show()
     model.set_saliency(False)
+
 
 
 def run(args, verbose=False):
@@ -520,11 +584,9 @@ def run(args, verbose=False):
             print(" - Context {}: {:.4f}".format(i + 1, acc))
         accs.append(acc)
           
-        ##Saliency
-        batch_size=args.batch
-        saliency_loader = get_data_loader(test_datasets, batch_size,cuda=cuda)
-        
-        create_saliency_map(model, saliency_loader, pred, i)
+    
+        ### Saliency
+        create_saliency_map(model, i, [0,1], 5)
 
     average_accs = sum(accs) / args.contexts
     if verbose:
