87dd8d23631b681a60a9258770b11b1fe6b79150
pricej18 <78972889+pricej18@users.noreply.github.com>
2024-10-19 11:25:17 -0400

    Update model_saver.py

--- model_saver.py
+++ model_saver.py
@@ -3,7 +3,6 @@
 import os
 import torch
 
-
 def save_model(model, save_path, task_id):
     """
     Save the model state dictionary and other relevant information.
@@ -13,13 +12,15 @@ def save_model(model, save_path, task_id):
         save_path (str): The directory where the model will be saved.
         task_id (int): The current task ID.
     """
+    # Create the "saved_models" directory inside the save_path if it does not exist
+    save_path = os.path.join(save_path, 'saved_models')
     if not os.path.exists(save_path):
-        os.makedirs(save_path) # Create the "Save_path" directory if it does not exist
+        os.makedirs(save_path)
 
-    # saves the model and assigns attributes to it (task_id, model_state_dict)
-    save_file = os.path.join(save_path, f'model_task_{task_id}.pth.tar') # The path where the model will be saved
+    # Save the model and assign attributes to it (task_id, model_state_dict)
+    save_file = os.path.join(save_path, f'model_task_{task_id}.pth.tar')
     torch.save({
-        'task_id': task_id,   # The task ID
-        'model_state_dict': model.state_dict(), # The model state dictionary
+        'task_id': task_id,
+        'model_state_dict': model.state_dict(),
     }, save_file)
-    print(f'Model for task {task_id} saved at {save_file}') # Print the path where the model is saved
+    print(f'Model for task {task_id} saved at {save_file}')
